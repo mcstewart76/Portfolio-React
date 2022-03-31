7d37825a161864c5641d62059c4cb98e8c79ae55
mcstewart76 <mcstewart76@gmail.com>
2022-03-31 00:22:54 -0400

    Added code to include routes and have them function with the navbar

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import About from './components/pages/About';
+import Works from './components/pages/Works';
+import Resume from './components/pages/Resume';
 import './index.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+    <Routes>
+      <Route
+        path="/About"
+        element={<About />}
+      />
+      <Route
+        path="/Works"
+        element={<Works />}
+      />
+      <Route
+        path="/Resume"
+        element={<Resume />}
+      />
+      
+    </Routes>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
