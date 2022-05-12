305a17a0d945ae1a197c7b783d1af6dae073c170
Chris Stewart <mcstewart76@gmail.com>
2022-05-12 09:25:14 -0400

    Updated resume page to fix state changes

--- client/src/components/pages/Resume.js
+++ client/src/components/pages/Resume.js
@@ -1,17 +1,25 @@
-import React  from 'react'
-import ResumeCS from '../pdf/CSResume.pdf'
+import React, {useState}  from 'react'
+
+
 
 export default function Resume() {
+
+   const [downloadState, downloaded] = useState("Download Resume Here!");
+
+    
     
     return (
         <div className="rounded container resumeCard" >
             <div className="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center resumeCard">
                 <div className="col-md-7 p-lg-5 mx-auto my-5">
-                    <h1 className="display-4 font-weight-normal" id="resDown">Download Resume Here!</h1>
-                    <a className="btn btn-outline-secondary me-1" href={ResumeCS} download onclick={() => {document.getElementById("resDown").innerText = "Resume Downloaded!"}}>Download Resume</a>
+                    <h1 className="display-4 font-weight-normal" id="resDown">{downloadState}</h1>
+                    <a className="btn btn-outline-secondary me-1" href="../pdf/ChrisStewartResume.pdf" download
+                     onClick={() => downloaded("Resume Downloaded!")}>
+                         Download Resume</a>
                     <a className="btn btn-outline-secondary ms-1" href="https://github.com/mcstewart76">GitHub Profile</a>
                 </div>
             </div>
         </div>
     )
+    
 }
