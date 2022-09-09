14b050c77708dd9e0615ae5d5feafa6a02f8c098
mcstewart76 <mcstewart76@gmail.com>
2022-09-08 23:52:24 -0400

    Changed photo and about page

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import coffeeImage from "../images/CS.jpg"
 
 export default function About() {
   return (
@@ -8,13 +9,13 @@ export default function About() {
         <div className=" mt-3 pt-2 rounded container-md container-sm  mx-auto text-center justify-content-center backgroundWhite"
          >
           <div className='pt-4'>
-          <img className="rounded box-shadow-dreamy mb-2 img-fluid mx-auto" src="https://avatars.githubusercontent.com/u/90533949?v=4"
+          <img className="rounded box-shadow-dreamy mb-2 img-fluid mx-auto coffeeImg" src={coffeeImage}
             alt='Me holding a coffee'></img>
           </div>
             <div className="col-md-7 pb-4 mx-auto mb-3 ">
 
               <h1 className="display-4 font-weight-normal">A little about me:</h1>
-              <p className="lead font-weight-normal ">&#x1F44A; I like to tinker and work with my hands.
+              <p className="lead font-weight-normal ">&#x1F44A; I enjoy coding, building, creating, and solving problems.
                 <br /> 😄 Fullstack Web Developer (MERN) 
                 <br /> 🛠 BS in Mechanical Engineering 
                 <br /> &#8749; Minor in Applied Mathematics 
